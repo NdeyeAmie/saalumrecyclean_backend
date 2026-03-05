ac361f9ca3491adb3cf037713af336bf2a66a253
Ndeye Amie Thiam <tndeyeamie456@gmail.com>
2026-03-05 10:10:33 +0000

    force redeploy

--- dash_admin/urls.py
+++ dash_admin/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 app_name = 'dash_admin'
-
+         
 urlpatterns = [
     path('',                             views.dashboard,         name='dashboard'),
     path('articles/ajout/',              views.ajout_article,     name='ajout_article'),
